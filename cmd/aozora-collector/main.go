82dfdb6426ead6799dd5256103d4ea93e20e22c0
kashmii <www.myac@gmail.com>
2023-09-28 23:21:38 +0900

    作品名とzipファイル名を取得

--- cmd/aozora-collector/main.go
+++ cmd/aozora-collector/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/url"
+	"path"
+	"regexp"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -16,6 +20,38 @@ type Entry struct {
 	ZipURL string
 }
 
+func findAuthorAndZIP(siteURL string) (string, string) {
+	log.Println("query", siteURL)
+	doc, err := goquery.NewDocument(siteURL)
+	if err != nil {
+		return "", ""
+	}
+
+	author := doc.Find("table[summary=作家データ] tr:nth-child(1) td:nth-child(2)").Text()
+
+	zipURL := ""
+	doc.Find("table.download a").Each(func(n int, elem *goquery.Selection) {
+		href := elem.AttrOr("href", "")
+		if strings.HasSuffix(href, ".zip") {
+			zipURL = href
+		}
+	})
+
+	if zipURL == "" {
+		return author, ""
+	}
+	if strings.HasPrefix(zipURL, "http://") || strings.HasPrefix(zipURL, "https://") {
+		return author, zipURL
+	}
+
+	u, err := url.Parse(siteURL)
+	if err != nil {
+		return author, ""
+	}
+	u.Path = path.Join(path.Dir(u.Path), zipURL)
+	return author, u.String()
+}
+
 func findEntries(siteURL string) ([]Entry, error) {
 	// URLからDOMオブジェクトを作成
 	doc, err := goquery.NewDocument(siteURL)
@@ -23,14 +59,33 @@ func findEntries(siteURL string) ([]Entry, error) {
 		return nil, err
 	}
 	// 処理
-	doc.Find("ol li a").Each(func(i int, elem *goquery.Selection) {
-		println(elem.Text(), elem.AttrOr("href", ""))
+	pat := regexp.MustCompile(`.*/cards/([0-9]+)/card([0-9]+).html$`)
+
+	entries := []Entry{}
+	doc.Find("ol li a").Each(func(n int, elem *goquery.Selection) {
+		token := pat.FindStringSubmatch(elem.AttrOr("href", ""))
+		if len(token) != 3 {
+			return
+		}
+		title := elem.Text()
+		pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html", token[1], token[2])
+		author, zipURL := findAuthorAndZIP(pageURL) // 作者とzipファイルのURLを取得
+		if zipURL != "" {
+			entries = append(entries, Entry{
+				AuthorID: token[1],
+				Author: author,
+				TitleId: token[2],
+				Title: title,
+				InfoURL: siteURL,
+				ZipURL: zipURL,
+			})
+		}
 	})
-	return nil, nil
+	return entries, nil
 }
 
 func main() {
-	listURL := "https://www.aozora.gr.jp/index_pages/person338.html"
+	listURL := "https://www.aozora.gr.jp/index_pages/person1346.html"
 
 	entries, err := findEntries(listURL)
 	if err != nil {
